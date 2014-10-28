9dd0cc7ff744cb849694df4ab65030581a04eb01
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-10-27 23:17:22 -0700

    Cleanin gup code and fixing bugs

--- classify/classifiers/abstract_classifier.py
+++ classify/classifiers/abstract_classifier.py
@@ -7,16 +7,16 @@ from abc import ABCMeta, abstractmethod
 
 
 class Classifier(object):
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def labels(self):
         pass
 
-	@abstractmethod
-	def train(self, training_examples):
-		pass
+    @abstractmethod
+    def train(self, training_examples):
+        pass
 
-	@abstractmethod
-	def test(self, test_examples):
-		pass
+    @abstractmethod
+    def test(self, test_examples):
+        pass
--- classify/classifiers/naive_bayes_sentiment.py
+++ classify/classifiers/naive_bayes_sentiment.py
@@ -14,42 +14,41 @@ from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 
+
 class NaiveBayesSentiment(Classifier):
     # Compress a likert scale with 7 values to one with 3.
-    @staticmethod
+    @classmethod
     def compress_likert(score):
         if score <= 3:
             return -1
-        else if score == 4:
+        elif score == 4:
             return 0
         else:
             return 1
 
-    @staticmethod
+    @classmethod
     def extract_docs_labels(examples):
         documents, labels = zip(*examples)
-        labels = map(compress_likert, labels)
+        labels = map(NaiveBayesSentiment.compress_likert, labels)
         return (documents, labels)
 
-    def labels:
+    def labels():
         return (-1, 0, 1)
 
     def train(self, training_examples):
         # Have: [ [Document, label], ... ]
         # Need: [[Features], ... ], [ label, ... ]
-        documents, labels = get_docs_labels(training_examples)
+        documents, labels = NaiveBayesSentiment.get_docs_labels(
+            training_examples)
         self.sentiment_clf = make_pipeline(
-                HashingVectorizer(stop_words='english'), 
-                MultinomialNB())
-
-        # Map likert scale to -1, 0, 1 ...
-        labels = map(compress_likert, labels)
+            HashingVectorizer(stop_words='english'),
+            MultinomialNB())
 
         # Train!
         self.sentiment_clf.fit(documents, labels)
 
     def test(self, test_examples):
-        documents, labels = get_docs_labels(test_examples)
+        documents, labels = NaiveBayesSentiment.get_docs_labels(test_examples)
         predictions = self.sentiment_clf.predict(documents)
         accuracy = np.mean(predictions == labels)
         return (zip(documents, predictions, labels), accuracy)
