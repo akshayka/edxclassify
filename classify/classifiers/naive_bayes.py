d356ced807ced423425481d73e5c3cb0481cb158
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-11-13 12:18:32 -0800

    fold custom_stop_word support into naive_bayes classifier

--- classify/classifiers/naive_bayes.py
+++ classify/classifiers/naive_bayes.py
@@ -11,6 +11,7 @@ TODO: docs and docs and docs
 
 '''
 from abstract_classifier import Classifier
+from custom_stop_words import CUSTOM_STOP_WORDS
 import clf_util
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
@@ -29,16 +30,19 @@ class NaiveBayes(Classifier):
 
 
     def make_clf(self):
+        stop_words='english'
+        if self.custom_stop_words:
+           stop_words=CUSTOM_STOP_WORDS 
         if self.use_tfidf:
             self.clf = make_pipeline(
                 CountVectorizer(token_pattern=self.token_pattern,
-                                stop_words='english'),
+                                stop_words=stop_words),
                 TfidfTransformer(),
                 MultinomialNB())
         else:
             self.clf = make_pipeline(
                 CountVectorizer(token_pattern=self.token_pattern,
-                                stop_words='english'),
+                                stop_words=stop_words),
                 MultinomialNB())
     
     def train(self, training_examples):
