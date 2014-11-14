73bb84f061f0bd9c957bd7ac7321c8d2687a5f3a
Shane Leonard <sw.leonard316@gmail.com>
2014-11-14 04:32:18 -0800

    Add process_doc in the cleaner for text preprocessing

--- classify/data_cleaners/dc_util.py
+++ classify/data_cleaners/dc_util.py
@@ -1,3 +1,8 @@
+import nltk
+from nltk.corpus import conll2000
+from chunk_parser import ChunkParser
+
+
 def compress_likert(score, binary=False, bin_threshold=4):
     if binary:
         if score <= bin_threshold:
@@ -11,3 +16,31 @@ def compress_likert(score, binary=False, bin_threshold=4):
         return 1
     else:
         return 2
+
+def upweight_first_sentence(document):
+    sentences = nltk.sent_tokenize(document)
+    document = ' '.join(sentences + [sentences[0]])
+    return document
+
+
+def extract_noun_phrases(document):
+    # Process the document into pos-tagged sentences
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+
+    # Build an 'NP' chunk parser
+    train_sents = conll2000.chunked_sents('train.txt', chunk_types=['NP'])
+    cp = ChunkParser(train_sents)
+
+    # Parse the document
+    parsed_doc = [cp.parse(sentence) for sentence in sentences]
+    result = []
+    for tree in parsed_doc:
+        for subtree in tree.subtrees():
+            if subtree.label() == 'NP':
+                result = result + [' '.join([w for (w,p) in subtree.leaves()])]
+
+    # For now, just upweight the noun phrases
+    document = ' '.join([document] + result)
+    return document
--- classify/data_cleaners/edx_confusion.py
+++ classify/data_cleaners/edx_confusion.py
@@ -6,15 +6,22 @@ class EdxConfusion(DataCleaner):
     def __init__(self, binary=False):
         self.name = 'EdxConfusion'
         self.binary = binary
-        
+
     def labels(self):
         if self.binary:
             return ['not-confused', 'confused']
         else:
             return ['knowledgeable', 'neutral', 'confused']
 
+    def process_doc(self, document):
+        document = document.lower()
+        # document = dc_util.extract_noun_phrases(document)
+        # document = dc_util.upweight_first_sentence(document)
+        return document
+
     # The first entry in each record is the document;
     # the sixth entry in each record is the likert score.
     def process_records(self, records):
-        return [(record[0], dc_util.compress_likert(int(float(record[5])),
-                self.binary, 4)) for record in records]
+        return [(self.process_doc(record[0]),
+                 dc_util.compress_likert(int(float(record[5])), self.binary, 4))
+                for record in records]
