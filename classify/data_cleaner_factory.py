233959e2033621ae4f539c47eefd52c9f90d69ec
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-11-18 16:24:36 -0800

    Add data cleaners for question, answer, opinion

--- classify/data_cleaner_factory.py
+++ classify/data_cleaner_factory.py
@@ -8,6 +8,9 @@ OUTPUT: subclass instance of DataCleaner
 from data_cleaners.edx_confusion import EdxConfusion
 from data_cleaners.edx_sentiment import EdxSentiment
 from data_cleaners.edx_urgency import EdxUrgency
+from data_cleaners.edx_answer import EdxAnswer
+from data_cleaners.edx_opinion import EdxOpinion
+from data_cleaners.edx_question import EdxQuestion
 
 
 def make_data_cleaner(dc, binary=False,
@@ -25,6 +28,18 @@ def make_data_cleaner(dc, binary=False,
                             first_sentence_weight)
     elif dc == 'edx_urgency':
         return EdxUrgency(binary, collapse_numbers,
+                          extract_noun_phrases,
+                          first_sentence_weight)
+    elif dc == 'edx_opinion':
+        return EdxOpinion(collapse_numbers,
+                          extract_noun_phrases,
+                          first_sentence_weight)
+    elif dc == 'edx_question':
+        return EdxQuestion(collapse_numbers,
+                          extract_noun_phrases,
+                          first_sentence_weight)
+    elif dc == 'edx_answer':
+        return EdxAnswer(collapse_numbers,
                           extract_noun_phrases,
                           first_sentence_weight)
     else:
