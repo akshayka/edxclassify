fc6ac9d1bda29ecbf98e8e378e0ed0747650f1b8
Akshay Agrawal <akshaykagrawal7@gmail.com>
2015-01-20 14:52:25 -0800

    remove experiment data

--- classify/tools/dump_posts.py
+++ classify/tools/dump_posts.py
@@ -1,10 +1,11 @@
-import pickle
-from data_cleaner_factory import make_data_cleaner
+import cPickle
+from classify.feature_spec import FEATURE_COLUMNS
 
-with open('../data/gold_sets/math_gold', 'rb') as infile:
-    dataset = pickle.load(infile)[1:]
+with open(raw_input('Enter data file: '), 'rb') as infile:
+    dataset = cPickle.load(infile)[1:]
     for record in dataset:
-        if '$' in record:
-        #if (record[5] > 4):
+        if float(record[FEATURE_COLUMNS['confusion']]) > 4.5:
             print record[0]
+            print record[FEATURE_COLUMNS['confusion']]
             raw_input('Press Enter to continue ...')
+
